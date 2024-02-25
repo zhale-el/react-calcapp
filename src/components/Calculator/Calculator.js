b7d01995ede6e5eaeed67a90a8798f8911340b23
zhaleh <zhalehelmi@gmail.com>
2024-02-25 17:16:51 +0330

    completed

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,28 +1,94 @@
+import { useState } from "react";
 import "./Calculator.css";
+import { evaluate } from "mathjs";
 
 function Calculator() {
+  let [result, setResult] = useState("");
+  let [hasDot, setHasDot] = useState(false);
+
+  let operators = ["-", "+", "*", "/"];
+  //==========================
+  const checkInput = (text) => {
+    if (text === "÷") return "/";
+    if (text === "×") return "*";
+    return text;
+  };
+
+  //==========================
+  const clickHandler = (e) => {
+    let input = checkInput(e.target.innerText);
+    if (input === ".") {
+      if (hasDot) return;
+      else {
+        setHasDot(true);
+      }
+    }
+
+    if (operators.includes(input)) {
+      setHasDot(false);
+    }
+
+    setResult(result + input);
+  };
+
+  //==========================
+  const clearBtn = () => {
+    setResult("");
+    setHasDot(false);
+  };
+
+  //==========================
+  const backapaceBtn = () => {
+    if (result.endsWith(".")) {
+      setHasDot(false);
+    }
+
+    setResult(result.slice(0, -1));
+  };
+
+  //==========================
+  const equalBtn = () => {
+    setResult(String(evaluate(result)));
+    setHasDot(false);
+  };
+
+  //==========================
   return (
     <div className="container">
-      <div className="screen"></div>
+      <div className="screen">{result}</div>
       <div className="buttons">
-        <button className="color twoCol">Clear</button>
-        <button className="color">C</button>
-        <button className="color">÷</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="color">×</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button className="color">-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className="color">+</button>
-        <button>0</button>
-        <button>.</button>
-        <button className="color twoCol">=</button>
+        <button onClick={clearBtn} className="color twoCol">
+          Clear
+        </button>
+        <button onClick={backapaceBtn} className="color">
+          C
+        </button>
+        <button onClick={clickHandler} className="color">
+          /
+        </button>
+        <button onClick={clickHandler}>7</button>
+        <button onClick={clickHandler}>8</button>
+        <button onClick={clickHandler}>9</button>
+        <button onClick={clickHandler} className="color">
+          ×
+        </button>
+        <button onClick={clickHandler}>4</button>
+        <button onClick={clickHandler}>5</button>
+        <button onClick={clickHandler}>6</button>
+        <button onClick={clickHandler} className="color">
+          -
+        </button>
+        <button onClick={clickHandler}>1</button>
+        <button onClick={clickHandler}>2</button>
+        <button onClick={clickHandler}>3</button>
+        <button onClick={clickHandler} className="color">
+          +
+        </button>
+        <button onClick={clickHandler}>0</button>
+        <button onClick={clickHandler}>.</button>
+        <button onClick={equalBtn} className="color twoCol">
+          =
+        </button>
       </div>
     </div>
   );
